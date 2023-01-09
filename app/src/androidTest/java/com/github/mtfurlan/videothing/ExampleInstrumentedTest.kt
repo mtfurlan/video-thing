f0495b60373c2b6d257fb588859edee0ca518f32
Mark Furland <mark@fur.land>
2023-01-09 13:25:52 -0500

    rsync binary test

--- app/src/androidTest/java/com/github/mtfurlan/videothing/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/mtfurlan/videothing/ExampleInstrumentedTest.kt
@@ -1,5 +1,9 @@
 package com.github.mtfurlan.videothing
 
+import android.util.Log
+import      java.io.BufferedReader
+import      java.io.InputStreamReader
+
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -8,6 +12,28 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
+
+import java.io.File;
+
+
+fun copyFromAssets(assetFile:String):File {
+    val appContext = InstrumentationRegistry.getInstrumentation().targetContext;
+    val outFile = File(appContext.getFilesDir(), assetFile)
+    if (!outFile.exists()) {
+        appContext.assets.open(assetFile).copyTo(outFile.outputStream())
+    }
+    return outFile;
+}
+
+fun logProcess(process:Process) {
+    var output: String = ""
+    val inputStream =  BufferedReader(InputStreamReader(process.getInputStream()))
+    while ( inputStream.readLine()?.also { output = it } != null) {
+        Log.d("FOO", "Debug: " + output)
+    }
+    inputStream.close()
+    process.waitFor()
+}
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -15,10 +41,49 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    private val TAG = "RSYNC TEST"
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.github.mtfurlan.videothing", appContext.packageName)
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext;
+        assertEquals("com.github.mtfurlan.videothing", appContext.packageName);
+
+
+        val dir = appContext.getExternalFilesDir("foo")
+        val file = File(dir, "somefile.txt")
+        file.printWriter().use { out ->
+            out.println("test")
+        }
+
+        val rsync = copyFromAssets("rsync")
+        rsync.setExecutable(true);
+        //val sshKey = copyFromAssets("skynet_rsa")
+
+        val binary = rsync.getAbsolutePath()
+        //val rshArg = "-rsh 'ssh -p 22222 -i " + sshKey.getAbsolutePath() + "'"
+        Log.e(TAG, binary.toString());
+        //Log.e(TAG, rshArg.toString());
+
+        val ls1 = ProcessBuilder("ls", "-l", binary)
+            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+            .start()
+        logProcess(ls1);
+        val ls2 = ProcessBuilder("ls", "-l", file.getAbsolutePath())
+            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+            .start()
+        logProcess(ls2);
+        //val process = ProcessBuilder(binary,
+        //                        rshArg,
+        //                        file.getAbsolutePath(),
+        //                        "mark@space.i3detroit.org:/tmp/")
+        //    .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+        //    .start()
+        //logProcess(process);
+
+
+        //deleting the file
+        file.delete();
+        System.out.println("file deleted");
+
     }
 }
